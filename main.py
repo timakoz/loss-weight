187a339eb05bed67cabf04a2c363ce335a1377e7
Timofey Kozlov <timakozlov07@gmail.com>
2021-10-13 22:43:42 +0300

    add print (day)

--- main.py
+++ main.py
@@ -11,17 +11,18 @@ import random
 p = 0
 lessed = 0
 needlesskkal = 5 * 7000
-c = 10
-while c > 0:
+c = 0
+while c < 10:
     if p == 3:
         z = 2000
     else:
         z = 2200
-    day = random.randint(1000,z)
-    if day > 2000:
+    daykkal = random.randint(1000,z)
+    print ('День', c, 'Сегодня нужно сьесть: ', daykkal)
+    if daykkal > 2000:
         p += 1
-    c -= 1
-    lessed += 2000 - day
+    c += 1
+    lessed += 2000 - daykkal
 
 lessed /= 7000
 
