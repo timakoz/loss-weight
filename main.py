6bcdeb353a38dc37f28415d57f9ba7159e63fc03
Timofey Kozlov <timakozlov07@gmail.com>
2021-10-17 18:16:04 +0300

    Co-authored-by: mblp <mblp.mblp@gmail.com>

--- main.py
+++ main.py
@@ -1,42 +1,39 @@
-#################
-#
-# TODO: add global var. to day and all weight also global while
-#
-#################
-import math
 import random
-
-
-
-p = 0
+x = 1000
+y = 0 # Дни когда Евдокия Артёмовна ест меньше 2000 ккал
+lessedall = 0
+p = 0 # Дни когда Евдокия Артёмовна ест больше 2000 ккал
 lessed = 0
 needlesskkal = 5 * 7000
 c = 1
-while c < 11:
-    if p == 2:
-        z = 1800
+while True:
+    if p == 2: # Если количество вкусняшек в 10 днях превышает 2 мы убираем шанс их выпадения
+        z = 1999
     else:
         z = 2200
-    daykkal = random.randint(1000,z)
+
+    if y == 9:  # Если нет вкусняшек в 9 днях мы принудительно их выдаем
+        x = 2000
+        z = 2200
+    else:
+        x = 1000
+    
+    daykkal = random.randint(x,z)
+
     print ('День', c, 'Сегодня нужно сьесть: ', daykkal)
-    if daykkal > 2000:
+    if daykkal >= 2000:
         p += 1
-    c += 1
-    lessed += 2000 - daykkal
-
-lessed /= 7000
-
-print ('Так держать вы сбросили:', round(lessed,2), 'кг', 'Осталось еще:', round(5 - lessed, 2), 'кг')
-
-
-
-
-
-
-
-
-
-
-
-
-#ordinarykkal = 2000 * days
+    else:
+        y += 1
+    c += 1 # текущий день
+    lessed += 2000 - daykkal # сколько сбросили ккал сегодня
+    
+    lessed /= 7000 # переводим в кг
+    lessedall = lessedall + lessed
+    if c % 10 == 1:
+        print ('Так держать! Вы уже сбросили:', round(lessedall,2), 'кг', 'Осталось еще:', round(5 - lessedall, 2), 'кг')
+        y = 0
+        p = 0
+    if lessedall >= 5:
+        print ('Поздравляю! Вы сбросили',round(lessedall, 2), 'кг' )
+        break
